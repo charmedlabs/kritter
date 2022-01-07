e8ec52963a6b140b3c25eddcb1340149c4d58762
rich legrand <rich@charmedlabs.com>
2022-01-07 13:51:25 -0600

    Add numpy array encoder/decoder for json

--- src/kritter/util.py
+++ src/kritter/util.py
@@ -1,5 +1,7 @@
 import os
 import logging
+import json
+import numpy as np
 
 def set_logger_level(logger, level):
     handler = logging.StreamHandler()
@@ -49,3 +51,51 @@ def valid_video_name(filename):
 
 def valid_media_name(filename):
     return valid_image_name(filename) or valid_video_name(filename)
+
+class JSONEncodeFromNumpy(json.JSONEncoder):
+    """
+    - Serializes python/Numpy objects via customizing json encoder.
+    - **Usage**
+        - `json.dumps(python_dict, cls=EncodeFromNumpy)` to get json string.
+        - `json.dump(*args, cls=EncodeFromNumpy)` to create a file.json.
+    """
+    def default(self, obj):
+        import numpy
+        if isinstance(obj, numpy.ndarray):
+            return {
+                "_kind_": "ndarray",
+                "_value_": obj.tolist()
+            }
+        if isinstance(obj, numpy.integer):
+            return int(obj)
+        elif isinstance(obj, numpy.floating):
+            return float(obj)
+        elif isinstance(obj,range):
+            value = list(obj)
+            return {
+                "_kind_" : "range",
+                "_value_" : [value[0],value[-1]+1]
+            }
+        return super(EncodeFromNumpy, self).default(obj)
+
+
+class JSONDecodeToNumpy(json.JSONDecoder):
+    """
+    - Deserilizes JSON object to Python/Numpy's objects.
+    - **Usage**
+        - `json.loads(json_string,cls=DecodeToNumpy)` from string, use `json.load()` for file.
+    """
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+
+    def object_hook(self, obj):
+        import numpy
+        if '_kind_' not in obj:
+            return obj
+        kind = obj['_kind_']
+        if kind == 'ndarray':
+            return numpy.array(obj['_value_'])
+        elif kind == 'range':
+            value = obj['_value_']
+            return range(value[0],value[-1])
+        return obj
