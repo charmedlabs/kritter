4f063a2024264f884b45b8c639190e7324cdbd23
rich legrand <rich@charmedlabs.com>
2022-02-11 13:53:58 -0600

    Add license header

--- src/kritter/kdropdownmenu.py
+++ src/kritter/kdropdownmenu.py
@@ -1,3 +1,13 @@
+#
+# This file is part of Kritter 
+#
+# All Kritter source code is provided under the terms of the
+# GNU General Public License v2 (http://www.gnu.org/licenses/gpl-2.0.html).
+# Those wishing to use Kritter source code, software and/or
+# technologies under different licensing terms should contact us at
+# support@charmedlabs.com. 
+#
+
 from kritter import Kcomponent
 from dash_devices import callback_context
 from dash_devices.dependencies import Input, Output, ALL
--- src/kritter/kterm/kterm.py
+++ src/kritter/kterm/kterm.py
@@ -1,3 +1,13 @@
+#
+# This file is part of Kritter 
+#
+# All Kritter source code is provided under the terms of the
+# GNU General Public License v2 (http://www.gnu.org/licenses/gpl-2.0.html).
+# Those wishing to use Kritter source code, software and/or
+# technologies under different licensing terms should contact us at
+# support@charmedlabs.com. 
+#
+
 #!/usr/bin/env python3
 from quart import Quart, Blueprint, websocket, copy_current_websocket_context, render_template, send_file
 from hypercorn.config import Config
