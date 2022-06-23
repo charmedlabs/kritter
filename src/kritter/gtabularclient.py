b05efe50319f7cd8e965de655e58cf95e96cadb3
kuhlkena <83791794+natureleif@users.noreply.github.com>
2022-06-23 12:00:29 -0500

    added ktabular client

---
+++ src/kritter/gtabularclient.py
@@ -0,0 +1,31 @@
+
+import os
+import gspread
+import gspread_dataframe as gd
+import pandas
+from .ktabularclient import KtabularClient
+
+class GtabularClient(KtabularClient):
+
+    def __init__(self,gcloud):
+        self.gcloud = gcloud
+        self.tabScopes = ['https://www.googleapis.com/auth/drive']
+        self.tabCreds = self.gcloud.creds()
+        self.tabClient = gspread.authorize(self.tabCreds)
+        
+    '''
+    Creates a google sheet with the requested data 
+    '''
+    def createGS(self,fileName,data):
+        self.sheet = self.tabClient.create(fileName)
+        sh = self.tabClient.open(fileName) # opens the created worksheet
+        worksheet = sh.sheet1 # goes to sheet1
+        gd.set_with_dataframe(worksheet, data) # sets the values in the worksheet using a panda data frame 
+        worksheet.format('A1:Z1', {'textFormat': {'bold': True}}) # makes headers bold
+        
+    """
+    returns the URL of the google sheet
+    """
+    def getURL(self):
+        spreadsheet_url = "https://docs.google.com/spreadsheets/d/%s" % self.sheet.id
+        return str(spreadsheet_url)
---
+++ src/kritter/ktabularclient.py
@@ -0,0 +1,16 @@
+class KtabularClient:
+
+    def __init__(self):
+        pass
+
+    '''
+    Creates a google sheet with the requested data 
+    '''
+    def createGS(self,fileName,USER,data): # data is a panda data frame
+        pass
+
+    '''
+    Gets the spreadsheet's URL and returns it as a string
+    '''
+    def getURL(self):
+        pass
