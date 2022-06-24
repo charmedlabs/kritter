be8b13bf3d2aa19cfaf000baff5bcf2ed3305144
kuhlkena <83791794+natureleif@users.noreply.github.com>
2022-06-24 13:02:38 -0500

    made changes in pull request

--- src/kritter/gtabularclient.py
+++ src/kritter/gtabularclient.py
@@ -10,19 +10,19 @@ class GtabularClient(KtabularClient):
 
     def __init__(self, gcloud):
         self.gcloud = gcloud
-        self.tabScopes = ['https://www.googleapis.com/auth/drive']
-        self.tabCreds = self.gcloud.creds()
-        self.tabClient = gspread.authorize(self.tabCreds)
+        self.tab_creds = self.gcloud.creds()
+        self.tab_client = gspread.authorize(self.tabCreds)
         
     '''
     Creates a google sheet with the requested data 
     '''
-    def createGS(self, fileName, data):
-        self.sheet = self.tabClient.create(fileName)
-        sh = self.tabClient.open(fileName) # opens the created worksheet
+    def create(self, fileName, data):
+        self.sheet = self.tab_client.create(fileName)
+        sh = self.tab_client.open(fileName) # opens the created worksheet
         worksheet = sh.sheet1 # goes to sheet1
         gd.set_with_dataframe(worksheet, data) # sets the values in the worksheet using a panda data frame 
         worksheet.format('A1:Z1', {'textFormat': {'bold': True}}) # makes headers bold
+        return(self.sheet)
         
     """
     returns the URL of the google sheet and makes the link public share
