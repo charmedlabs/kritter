5d9e178dde650e554cef507954f31567dd7adbdb
kuhlkena <kuhlkena@whitman.edu>
2022-06-28 10:18:08 -0500

    added new method definitions and changed the data argument in create()  to be optional

--- src/kritter/gtabularclient.py
+++ src/kritter/gtabularclient.py
@@ -2,7 +2,7 @@
 import os
 import gspread
 import gspread_dataframe as gd
-import pandas
+import pandas as pd
 from .ktabularclient import KtabularClient
 
 
@@ -16,18 +16,49 @@ class GtabularClient(KtabularClient):
     '''
     Creates a google sheet with the requested data and returns the sheet
     '''
-    def create(self, fileName, data):
-        sheet = self.tab_client.create(fileName)
-        sh = self.tab_client.open(fileName) # opens the created worksheet
+    def create(self, title, data = pd.DataFrame()):
+        sheet = self.tab_client.create(title)
+        sh = self.tab_client.open(title) # opens the created worksheet
         worksheet = sh.sheet1 # goes to sheet1
-        gd.set_with_dataframe(worksheet, data) # sets the values in the worksheet using a panda data frame 
-        worksheet.format('A1:Z1', {'textFormat': {'bold': True}}) # makes headers bold
+        if not data.empty:
+            gd.set_with_dataframe(worksheet, data) # sets the values in the worksheet using a panda data frame 
+            worksheet.format('A1:Z1', {'textFormat': {'bold': True}}) # makes headers bold
         return(sheet)
         
     """
     returns the URL of the google sheet and makes the link public share
     """
     def get_url(self,sheet):
-        sheet.share(None,perm_type = 'anyone', role = 'writer', with_link = True)
+        sheet.share(None, perm_type = 'anyone', role = 'writer', with_link = True)
         spreadsheet_url = "https://docs.google.com/spreadsheets/d/%s" % sheet.id
         return str(spreadsheet_url)
+
+    '''
+    looks up the sheet by name and if it exists returns a referance, otherwise returns None
+    '''
+    def lookup(self, title):
+        pass
+
+    '''
+    changes the value of a cell in the requested worksheet
+    '''
+    def edit_cell(self, sheet, row, col):
+        pass
+
+    '''
+    append data adds data to the sheet without overwriting existing data
+    '''
+    def append_data(self, sheet, data):
+        pass
+
+    '''
+    deletes the requested sheet by name as string
+    '''
+    def delete(self, sheet):
+        pass
+
+    '''
+    renames the requested sheet 
+    '''
+    def rename(self, sheet, title):
+        pass
--- src/kritter/ktabularclient.py
+++ src/kritter/ktabularclient.py
@@ -7,11 +7,41 @@ class KtabularClient:
     '''
     Creates a google sheet with the requested data and returns the sheet
     '''
-    def create(self, fileName, data): # data is a panda data frame
+    def create(self, title, data = None): # data is a panda data frame
         pass
 
     '''
     returns the URL of the google sheet and makes the link public share
     '''
     def get_url(self, sheet):
+        pass
+
+    '''
+    looks up the sheet by name and if it exists returns a referance, otherwise returns None
+    '''
+    def lookup(self, title):
+        pass
+
+    '''
+    changes the value of a cell in the requested worksheet
+    '''
+    def edit_cell(self, sheet, row, col):
+        pass
+
+    '''
+    append data adds data to the sheet without overwriting existing data
+    '''
+    def append_data(self, sheet, data):
+        pass
+
+    '''
+    deletes the requested sheet by name as string
+    '''
+    def delete(self, sheet):
+        pass
+
+    '''
+    renames the requested sheet 
+    '''
+    def rename(self, sheet, title):
         pass
