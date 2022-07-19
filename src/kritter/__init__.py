603e4ade9ddfe82890b63e783525be0b63c75746
Matt Peregoy <peregoywork@gmail.com>
2022-07-19 12:33:58 -0400

    changing telegram to texting package init

--- src/kritter/__init__.py
+++ src/kritter/__init__.py
@@ -40,7 +40,7 @@ from .gcloud import Gcloud
 from .gpstoremedia import GPstoreMedia
 from .savemediaqueue import SaveMediaQueue
 from .proxy import Proxy 
-from .telegramclient import TelegramClient
+from .textingclient import TextingClient
 from .configfile import ConfigFile
 from .importconfig import import_config
 
