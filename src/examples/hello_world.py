9dc7c8287b0546eb457e1b512153f819c3567255
Matt Peregoy <peregoywork@gmail.com>
2022-11-06 13:28:38 -0500

    skeleton of example

--- src/examples/hello_world.py
+++ src/examples/hello_world.py
@@ -4,3 +4,14 @@ Hello world with a button.
 press button, prints counter and message to console
 kapp.layout = [“hello world”, button]
 '''
+
+import kritter
+
+if __name__ == "__main__":
+    kapp = kritter.Kritter()    # Create Kritter server
+    style = {"label_width": 3, "control_width": 6}  # general style for page
+
+    button = kritter.Kbutton()
+        
+    kapp.layout = ["hello world", button]
+
