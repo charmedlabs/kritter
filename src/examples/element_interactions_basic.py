63cdd2d41b862b8862335fcd67274e670357cb7f
Matt Peregoy <peregoywork@gmail.com>
2022-11-07 22:44:39 -0500

    very basic skeleton - no compents yet

--- src/examples/element_interactions_basic.py
+++ src/examples/element_interactions_basic.py
@@ -7,3 +7,21 @@ message = 'print from 'element name'
 dropdown and checklist elements have a list of bogus options 
 - 3 or 4 colors for example
 '''
+
+# imports
+import kritter
+
+if __name__ == "__main__":
+
+    # Create Kritter server before defining compenents
+    kapp = kritter.Kritter()
+
+    # Application Styles
+    main_style = {"label_width": 3, "control_width": 6}
+
+    # Contents/Components of Application
+
+
+    kapp.layout = []    # define interface layout
+    kapp.run()  # Run Server - vizy.local:5000/
+
