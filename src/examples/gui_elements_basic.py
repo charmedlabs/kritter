132a14cd4575807204ab3ed197454a9ae8168e58
Matt Peregoy <peregoywork@gmail.com>
2022-11-07 22:44:19 -0500

    skeleton setup

--- src/examples/gui_elements_basic.py
+++ src/examples/gui_elements_basic.py
@@ -1,13 +1,33 @@
 '''
-1 kbutton, 1 ktextbox, and 3 kdropdown menus: 
+3 kdropdown menus, 1 kbutton, 1 ktextbox: 
 - appear/disappear dropdown causes button to appear and disappear. 
 - enabled/disabled causes button to be enabled/disabled.  
 - spinner/no spinner causes the button to show spinner, not show spinner. 
-
-ktextbox, if it's not empty (""), creates a new browser tab with the URL in the textbox.
-notes:
-- Href property (grep on href, vizy apps) 
-- External site
-- Target = _
-- Motionscope app line 73 “options” variable
+- when the button is clicked, if ktextbox not empty, will create a new browser tab with the URL from the textbox value.
 '''
+
+# imports
+import kritter
+import dash_bootstrap_components as dbc
+
+if __name__ == "__main__":
+
+    # Create Kritter server before defining compenents
+    kapp = kritter.Kritter()
+
+    # Application Styles
+    main_style = {"label_width": 3, "control_width": 6}
+
+    # Contents/Components of Application
+
+    options = [dbc.DropdownMenuItem(k, id=kapp.new_id(), href="export/"+v[0], target="_blank", external_link=True) for k, v in self.export_map.items()]
+    # We don't want the export funcionality to be shared! (service=None)
+    export = kritter.KdropdownMenu(name="Export data", options=options, service=None)
+
+    dropdown_menu_main = kritter.KdropdownMenu()
+    # dropdown_menu = kritter.KdropdownMenu()
+    # dropdown_menu = kritter.KdropdownMenu()
+    textbox = kritter.KtextBox(placeholder="Type Here!")
+
+    kapp.layout = [dropdown_menu_main]    # define interface layout
+    kapp.run()  # Run Server - vizy.local:5000/
