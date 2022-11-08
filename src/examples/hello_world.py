90fc8ec01e23aae241226accc6e268d54204bad4
Matt Peregoy <peregoywork@gmail.com>
2022-11-07 21:40:47 -0500

    complete - HelloWorld with incremental button

--- src/examples/hello_world.py
+++ src/examples/hello_world.py
@@ -1,17 +1,45 @@
 '''
-Hello world with a button.
+Hello World with a button.
 “hello world” at top of page
 press button, prints counter and message to console
 kapp.layout = [“hello world”, button]
 '''
 
+# imports
 import kritter
 
+html_index = f'''
+<html>
+    <h4>Hello World!</h4>
+    <p>Button has been clicked _x_ times</p>
+</html>
+'''
+
+counter = 0
+
 if __name__ == "__main__":
-    kapp = kritter.Kritter()    # Create Kritter server
-    style = {"label_width": 3, "control_width": 6}  # general style for page
 
-    button = kritter.Kbutton()
-        
-    kapp.layout = ["hello world", button]
+    # Create Kritter server - server is created before components
+    kapp = kritter.Kritter()    
 
+    # Application Styles
+    main_style = {"label_width": 3, "control_width": 6}
+
+    # Contents/Components of Application
+    header = kritter.Ktext(value="Hello World!", style=main_style)
+    display = kritter.Ktext(value="", style=main_style)   # empty text box until button action
+    # Incrementor Button
+    button_icon = kritter.Kritter.icon("save")
+    button = kritter.Kbutton(name=[button_icon, "Incrementor"])
+
+    # Define Callbacks for Components
+    @button.callback()
+    def func():
+        global counter  # accessing global variable
+        counter += 1    # increment counter before display
+        message = f"You have said Hello! ({counter}) number of times"
+        mods = display.out_value(message)   # update contents of 'display' compenent
+        kapp.push_mods(mods)                # apply updates to app
+
+    kapp.layout = [header, display, button] # define interface layout
+    kapp.run()  # Run Server - vizy.local:5000/
