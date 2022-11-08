d0187016d90a24d67d55ab9c7a57e40d577b285d
Matt Peregoy <peregoywork@gmail.com>
2022-11-08 11:52:13 -0500

    add all elements and their callbacks

--- src/examples/element_interactions_basic.py
+++ src/examples/element_interactions_basic.py
@@ -20,6 +20,38 @@ if __name__ == "__main__":
     main_style = {"label_width": 3, "control_width": 6}
 
     # Contents/Components of Application
+    checkbox = kritter.Kcheckbox()
+    checklist = kritter.Kchecklist()
+    dropdownMenu = kritter.KdropdownMenu()
+    dropdown = kritter.Kdropdown()
+    slider = kritter.Kslider()
+    textBox = kritter.KtextBox()
+    text = kritter.Ktext()
+
+    # Define Callbacks
+    checkbox.callback()
+    def func():
+        pass
+
+    checklist.callback()
+    def func():
+        pass
+
+    dropdownMenu.callback()
+    def func():
+        pass
+
+    dropdown.callback()
+    def func():
+        pass
+
+    slider.callback()
+    def func():
+        pass
+
+    textBox.callback()
+    def func():
+        pass
 
 
     kapp.layout = []    # define interface layout
